bab70d50de1427bf2e6a5c38c2cc7b9e28eaa477
Jahxrxo <jahirr31@gmail.com>
2026-02-13 14:55:10 -0600

    .

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Jahxr\Desktop\Agedacion\backend agendacion de citas\clinica-backend\venv
+set VIRTUAL_ENV=C:\Users\Jahxr\Desktop\agendacion\clinicabackend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
